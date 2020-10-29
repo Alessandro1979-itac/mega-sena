de362318f9ac74ea283bb6bbe18aad73b9121179
Alessandro <sandromuniz27@hotmail.com>
2020-10-29 15:49:43 -0400

    Add components PickedNumbers index e styles

---
+++ src/components/PickedNumbers/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #01ac5d;
+  height: 8vh;
+  padding: 12px;
+  font-size: 30px;
+  font-weight: bold;
+  color: #fff;
+  strong {
+    margin-right: 80px;
+  }
+  span {
+    padding: 10px;
+    background: radial-gradient(circle at top left, #ffffff 50%, #708090);
+    color: #000;
+    border-radius: 50%;
+    margin-right: 40px;
+  }
+`;
