85bd517b519b505a0830d1fd6865f61c8530e15e
Alessandro <sandromuniz27@hotmail.com>
2020-10-29 15:51:08 -0400

    Add pages App e styles

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import Form from '../components/Form';
+import Numbers from '../components/Numbers';
+import PickedNumbers from '../components/PickedNumbers';
+import Header from '../components/Header';
+import { Container } from './styles';
+
+function getEmptyArray() {
+   const array = Array.from({length: 60}).map((_, index) => {
+     const id = index + 1;
+     
+     const description = id.toString().padStart(2, '0');
+     
+     return {
+       id,
+       description,
+       value: id,
+       count: 0,
+     };
+    })
+    return array;
+}
+
+function generateNumber(from = 1, to = 60) {
+  return Math.max(Math.ceil(Math.random() * to), from);
+}
 
 function App() {
+  const [numbers, setNumbers] = useState(getEmptyArray());
+  const [pickedNumbers, setPickedNumbers] = useState([]);
+  const [isCalculating, setIsCalculating] = useState(false);
+  const [limit, setLimit] = useState(1);
+
+  const canRun = useRef(false);
+
+  useEffect(() => {
+    if(!canRun.current) {
+      return;
+    }
+    const interval = setInterval(() => {
+      if(pickedNumbers.length === 6) {
+        setIsCalculating(false);
+        return;
+      }
+
+      const newNumber = generateNumber();
+      const newNumbers = [...numbers];
+      const newPickedNumbers = [...pickedNumbers];
+
+      const item = newNumbers.find(item => item.value === newNumber);
+      item.count++;
+
+      if(item.count === limit) {
+        newPickedNumbers.push(item.value)
+      } 
+
+      setNumbers(newNumbers);
+      setPickedNumbers(newPickedNumbers);
+      
+    }, 4);
+
+    return () => clearInterval(interval);
+  }, [isCalculating, limit, numbers, pickedNumbers]);
+
+  const handleLimitChange = (newLimit) => {
+    setLimit(newLimit);
+  }
+
+  const handleShuffleClick = () => {
+    setNumbers(getEmptyArray());
+    setPickedNumbers([]);
+    setIsCalculating(true);
+    canRun.current = true;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header/>
+
+      <Form 
+        data={{limit, isCalculating}} 
+        onLimitChange={handleLimitChange}
+        onShuffle={handleShuffleClick}
+      />
+
+      <Numbers numbers={numbers} pickedNumbers={pickedNumbers}/>
+      
+      <PickedNumbers pickedNumbers={pickedNumbers}/>
+    </Container>
   );
 }
 
--- src/pages/styles.js
+++ src/pages/styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    width: 1000px;
-    height: 100vh;
-    margin: 0 auto;
-    background: '#fefedc';
+  width: 1000px;
+  height: 100vh;
+  margin: 0 auto;
+  background: #fefedc;
 `;
